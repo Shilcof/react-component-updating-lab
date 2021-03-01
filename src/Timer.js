d488c6a7539236fef71ab2f4d390478eedfc9c66
Shilcof <robert.shilcof@gmail.com>
2021-03-01 11:36:55 +0000

    commit tree 2cffea9127c56026ef6344932cd6ddddea4f653e

--- src/Timer.js
+++ src/Timer.js
@@ -11,7 +11,6 @@ class Timer extends Component {
   }
 
   //Your code here
-
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
@@ -19,8 +18,16 @@ class Timer extends Component {
     );
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    return (this.state.time !== nextState.time)
+  }
+
   componentWillUnmount() {
     clearInterval(this.interval);
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
   render() {
